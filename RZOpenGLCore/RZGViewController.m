5bc0ef2775feec2444b82d1c0b2fdac15bd5468c
John Stricker <john.stricker@raizlabs.com>
2014-04-17 08:47:19 -0400

    folder structure for core

--- RZOpenGLCore/RZGViewController.m
+++ RZOpenGLCore/RZGViewController.m
@@ -1,9 +1,9 @@
 //
 //  RZGViewController.m
-//  RZOpenGLCoreSetup
+//  RZGLCoreDevelopemnt
 //
-//  Created by John Stricker on 4/12/14.
-//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//  Created by John Stricker on 4/17/14.
+//  Copyright (c) 2014 John Stricker. All rights reserved.
 //
 
 #import "RZGViewController.h"
@@ -14,36 +14,10 @@
 
 @implementation RZGViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
